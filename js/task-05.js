296b6aeafa21c54cdabc87db91cf7f817bcbc66f
Maserto <108365716+Maserto@users.noreply.github.com>
2022-11-03 20:26:44 +0200

    5 and 8

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,9 @@
 const inputEl = document.querySelector("input#name-input");
 const outputEl = document.querySelector("span#name-output");
 
-const insertInputIntoOutput = () => inputEl.value = outputEl.textContent;
+const insertInputIntoOutput = (event) => {return inputEl.value.length === 0
+    ? outputEl.textContent = 'Anonymous'
+    : outputEl.textContent = event.currentTarget.value;}
 
-// inputEl.addEventListener(input, insertInputIntoOutput);
-console.log(inputEl)
+inputEl.addEventListener('input', insertInputIntoOutput);
+// console.log(inputEl)
